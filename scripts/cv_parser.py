bd1a39bfc0a3c2e45ad73be2ecbb7daace1b5bc3
avouacr <avouacr@gmail.com>
2020-04-04 00:08:03 +0200

    Start preprocessing

--- scripts/cv_parser.py
+++ scripts/cv_parser.py
@@ -6,22 +6,32 @@ import pdfminer
 from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
 from pdfminer.converter import TextConverter
 from pdfminer.pdfpage import PDFPage
+import spacy
+import fr_core_news_md
 
-cv_path = 'cv_fr_1p.pdf'
+
+nlp = fr_core_news_md.load()
 
 
 def cv_to_text(cv_path):
     """
+    Extract raw text from a (pdf) resume.
 
     Sources :
     https://dzone.com/articles/exporting-data-from-pdfs-with-python
     https://gist.github.com/terencezl/61fe3f28c44a763dd1e9f060b8ff6f2e
     https://www.blog.pythonlibrary.org/2018/05/03/exporting-data-from-pdfs-with-python/
 
-    :param cv_path:
-    :return:
+    Parameters
+    ----------
+    cv_path (str): Path of the resume as a pdf file.
+
+    Returns
+    -------
+    str: Raw text from the resume.
+
     """
-    assert os.path.splitext(cv_path)[-1] == '.pdf', 'CV should be a pdf file.'
+    assert os.path.splitext(cv_path)[-1] == '.pdf', 'The resume should be a pdf file.'
 
     # Perform layout analysis for all text
     laparams = pdfminer.layout.LAParams()
@@ -43,3 +53,32 @@ def cv_to_text(cv_path):
 
     if text:
         return text
+
+
+def TextPreprocessor():
+
+    def __init__(self, cv_text):
+        self.cv_text = cv_text
+        self.doc = nlp(cv_text)
+
+    def remove_named_entities(self):
+
+
+
+
+
+
+CV_NAME = 'romain'
+CV_DIR = 'cv_tests'
+CV_PATH = os.path.join(CV_DIR, CV_NAME) + '.pdf'
+
+cv_text = cv_to_text(CV_PATH)
+
+
+
+doc = nlp(cv_text)
+for ent in doc.ents:
+
+
+
+
