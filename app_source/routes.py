0edf1bf6e3cf5dc5a0dbb553faf07343962a6d67
avouacr <avouacr@gmail.com>
2020-04-12 20:03:06 +0200

    Login improved.

--- app_source/routes.py
+++ app_source/routes.py
@@ -1,10 +1,15 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app_source import app
 from app_source.forms import LoginForm
 
 
-@app.route('/')
-@app.route('/login')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     login_form = LoginForm()
+    if login_form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            login_form.username.data, login_form.remember_me.data))
     return render_template('login.html', title='Identification', form=login_form)
+
+# TODO: change login error messages to French
