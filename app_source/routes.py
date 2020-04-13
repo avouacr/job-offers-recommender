4651bc2a39b7f3a1c2a101116db5e1859694da19
avouacr <avouacr@gmail.com>
2020-04-13 17:34:39 +0200

    Add logout button

--- app_source/routes.py
+++ app_source/routes.py
@@ -44,7 +44,7 @@ def login():
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('main'))
+    return redirect(url_for('login'))
 
 
 @app.route('/register/', methods=['GET', 'POST'])
