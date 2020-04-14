85758347e1826292bb2cb0ee5594c7c947d51f91
JCCen <jaimecostacentena@gmail.com>
2020-04-14 16:39:20 +0200

    remove cached repo

--- CV_builder_draft/cv_generator-master/cv_generator/themes/__init__.py
+++
@@ -1,2 +0,0 @@
-from .developer import ThemeDeveloper
-from .sitges import ThemeSitges
--- CV_builder_draft/cv_generator-master/cv_generator/utils.py
+++
@@ -1,7 +0,0 @@
-_latex_special_chars = {
-    '%': r'\%',
-}
-
-
-def escape_link(href):
-    return ''.join(_latex_special_chars.get(c, c) for c in str(href))
