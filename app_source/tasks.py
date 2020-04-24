aa0ec8d7bcc95025cefbec0d1208bc420c380bc3
avouacr <avouacr@gmail.com>
2020-04-24 19:53:46 +0200

    CLean repo

--- app_source/tasks.py
+++
@@ -1,9 +0,0 @@
-
-import fasttext.util
-
-def import_fasttext():
-    """Download and extract FastText pre-trained word vectors."""
-    print('Downloading and extracting FastText French model.')
-    fasttext.util.download_model('fr', if_exists='ignore')
-    print('FastText model ready to use.')
-    return True
