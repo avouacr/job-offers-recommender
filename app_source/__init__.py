6713ac7de00992e7d26e73c8512dc5211a9cb57a
avouacr <avouacr@gmail.com>
2020-04-24 15:20:08 +0200

    Auto initialization of db

--- app_source/__init__.py
+++ app_source/__init__.py
@@ -1,8 +1,6 @@
 from flask import Flask
 from config import Config
 from flask_login import LoginManager
-# from redis import Redis
-# import rq
 
 
 app = Flask(__name__)
@@ -14,10 +12,8 @@ login.login_view = 'login' # Make access to the app only possible to logged in u
 # Import routes and models at the bottom to avoid circular imports
 from app_source import routes, models, errors, tasks
 models.db.init_app(app) # Connect database to the app
-
-# Download FastText model in background
-# queue = rq.Queue('app-tasks', connection=Redis.from_url('redis://'))
-# job = queue.enqueue(tasks.import_fasttext)
+models.db.create_all() # Initialize tables if they don't already exist
+models.db.session.commit()
 
 app.static_folder = 'static'
 
@@ -32,8 +28,3 @@ def populate_db_test():
     """Populate db with test data."""
     models.populate_db_test()
     print('Database populated with toy data.')
-
-# @app.cli.command('update_job_offers')
-# def update_job_offers():
-#     """Update job offers using newly scraped data."""
-#     models.update_job_offers()
